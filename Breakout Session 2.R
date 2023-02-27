b027ac782aacdbb57b384fe545dee682a5b2ce57
Jane Doe <jane@example.com>
2023-02-27 09:03:12 +0100

    Second try

--- Breakout Session 2.R
+++ Breakout Session 2.R
@@ -4,3 +4,26 @@ library(tidyverse)
 library(spotifyr)
 library(compmus)
 
+Californication <-
+  get_tidy_audio_analysis("34KTEhpPjq6IAgQg2yzJAL?") |>
+  select(segments) |>
+  unnest(segments) |>
+  select(start, duration, pitches)
+
+Californication |>
+  mutate(pitches = map(pitches, compmus_normalise, "chebyshev")) |>
+  compmus_gather_chroma() |> 
+  ggplot(
+    aes(
+      x = start + duration / 2,
+      width = duration,
+      y = pitch_class,
+      fill = value
+    )
+  ) +
+  geom_tile() +
+  labs(x = "Time (s)", y = NULL, fill = "Magnitude") +
+  theme_minimal() +
+  scale_fill_viridis_c()
+
+
--- The One.R
+++ The One.R
@@ -9,6 +9,7 @@ eighties_playlist <- get_playlist_audio_features("", "37i9dQZF1DX4UtSsGT1Sbe?")
 nineties_playlist <- get_playlist_audio_features("", "37i9dQZF1DXbTxeAdrVG2l")
 zeros_playlist <- get_playlist_audio_features("", "37i9dQZF1DX4o1oenSJRJd?")
 tens_playlist <- get_playlist_audio_features("", "37i9dQZF1DX5Ejj0EkURtP?")
+twenties_playlist <- get_playlist_audio_features("", "4vSTV61efRmetmaoz95Vet?")
 
 All_playlist <-
   bind_rows(
@@ -16,17 +17,18 @@ All_playlist <-
     eighties_playlist |> mutate(category = "Eighties"),
     nineties_playlist |> mutate(category = "Nineties"),
     zeros_playlist |> mutate(category = "Zeros"),
-    tens_playlist |> mutate(category = "Tens")
+    tens_playlist |> mutate(category = "Tens"),
+    twenties_playlist |> mutate(category = "Twenties")
   )
 
 Arranged_playlists <- All_playlist %>%
   arrange(playlist_name)
 
-PLaylist_labs <- c("Seventies", "Eighties", "Nineties", "Zeros", "Tens")
+PLaylist_labs <- c("70s", "80s", "90s", "00s", "10s", "20s")
 
 Plot_Arranged_playlists <- ggplot(Arranged_playlists, aes(playlist_name, loudness)) +
   geom_boxplot() +
-  scale_x_discrete(limits=c("All Out 70s", "All Out 80s", "All Out 90s", "All Out 2000s", "All Out 2010s"),
+  scale_x_discrete(limits=c("All Out 70s", "All Out 80s", "All Out 90s", "All Out 2000s", "All Out 2010s", "All Out 2020s"),
                    labels= PLaylist_labs) +
   # Theme
   theme_classic() +
@@ -43,3 +45,16 @@ Plot_Arranged_playlists <- ggplot(Arranged_playlists, aes(playlist_name, loudnes
 
 # plotly
 ggplotly(Plot_Arranged_playlists)
+
+
+
+
+## Plot 2
+
+Mean_playlists <- Arranged_playlists %>%
+  group_by(playlist_name) %>%
+  summarise(mean_loudness = mean(loudness))
+
+ggplot(Mean_playlists, aes(playlist_name, mean_loudness) +
+         geom_point()
+
